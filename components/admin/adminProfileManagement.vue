97af4d81fa48bbec86ca17e6fbcbf1c7bea2db7b
emirrssen <emirrssen@gmail.com>
2023-12-18 17:14:22 +0300

    General concept design cretaed.

---
+++ components/admin/adminProfileManagement.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex flex-column" style="gap: 20px;">
+                <PersonelInfoDetail></PersonelInfoDetail>
+            </div>
+            <div class="col-6 d-flex justify-content-center align-items-center">
+                <ChangePassword></ChangePassword>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+    * {
+        box-shadow: none;
+    }
+</style>
---
+++ stores/admin/adminStore.ts
@@ -0,0 +1,32 @@
+import { useToast } from "vue-toastification";
+
+export const useAdminStore = defineStore('admin-store', () => {
+    const toast = useToast();
+    const listItems = ref([
+        { Id: 1, Title: "Kullanıcı Yönetimi", IsSelected: true },
+        { Id: 2, Title: "Kitap Yönetimi", IsSelected: false },
+        { Id: 3, Title: "Yazar Yönetimi", IsSelected: false },
+        { Id: 4, Title: "Kategori Yönetimi", IsSelected: false },
+        { Id: 5, Title: "Ödünç Alma İşlemleri", IsSelected: false },
+        { Id: 6, Title: "Personel Yönetimi", IsSelected: false },
+        { Id: 7, Title: "Pano Yönetimi", IsSelected: false },
+        { Id: 8, Title: "Kişisel Hesap Yönetimi", IsSelected: false },
+    ]);
+    const selectedPanel = ref(0);
+
+    function selectPanel(selectedPanelId: number) {
+        for (const item of listItems.value) {
+            if (item.Id !== selectedPanelId) {
+                item.IsSelected = false;
+            } else {
+                item.IsSelected = true;
+                selectedPanel.value = selectedPanelId;
+            }
+        }
+    }
+
+    return {
+        listItems, selectedPanel,
+        selectPanel
+    }
+})
